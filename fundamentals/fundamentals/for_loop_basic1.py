4b3d5d48c2b6ff8d3c05234b45fed3d1f397394d
JonSolo1417 <Jonathonsmth@yahoo.com>
2021-07-05 21:48:47 -0400

    loops

---
+++ fundamentals/fundamentals/for_loop_basic1.py
@@ -0,0 +1,31 @@
+# for x in range (0,151):
+#     print(x)
+
+# for x in range (5, 1001,5):
+#     print(x)
+
+# for x in range(1,101):
+#     if x % 5 == 0 and x % 10 != 0:
+#         print("coding")
+#     elif x % 10 == 0:
+#         print("coding dojo")
+#     else:
+#         print(x)
+
+# count = 0
+
+# for x in range (0,500001):
+#     if x % 2 != 0:
+#         count += x
+# print(count)
+
+# for x in range (2018,0,-4):
+#     print(x)
+
+lowNum=int(3)
+highNum=int(50)
+mult=int(4)
+
+for x in range (lowNum,highNum):
+    if x % mult == 0:
+        print(x)
